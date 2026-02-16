3bbd7a779f69539df8a5f6a89cd9b16f0d2df2fe
hajam09 <hajam09@yahoo.com>
2026-02-16 21:01:58 +0000

    fix nav for mobile

--- core/templatetags/custom_filters.py
+++ core/templatetags/custom_filters.py
@@ -4,14 +4,14 @@ from django.urls import reverse
 register = template.Library()
 
 
-def linkItem(name, url, sub_links=None):
-    return {'name': name, 'url': url, 'subLinks': sub_links}
+def linkItem(name, url=None, icon=None, subLinks=None):
+    return {'name': name, 'url': url, 'icon': icon, 'subLinks': subLinks or []}
 
 
 @register.simple_tag
 def navigationPanel(request):
     links = [
-        linkItem('Dashboards', None, [
+        linkItem('Dashboards', subLinks=[
             linkItem('Admin', reverse('admin:index')),
             linkItem('Cat Purchases', reverse('core:cat-purchases-dashboard')),
             linkItem('Energy Payments', reverse('core:energy-payments-dashboard')),
